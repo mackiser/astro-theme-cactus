1d32857f27d52a293f1b0060bc8fefa9e12c9887
mackiser <mkiser96@gmail.com>
2024-09-16 20:33:04 -0400

    changed index.astro and change code block theme

--- src/site.config.ts
+++ src/site.config.ts
@@ -71,6 +71,6 @@ export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
 		return `[data-theme="${theme.name}"]`;
 	},
 	// One dark, one light theme => https://expressive-code.com/guides/themes/#available-themes
-	themes: ["monokai", "everforest-light"],
+	themes: ["andromeeda", "everforest-light"],
 	useThemedScrollbars: true,
 };
